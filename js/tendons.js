5a80cbfe40e4f22499a1af84d7e2ae3f36f7a034
Artyom <37940084+Gohadouken@users.noreply.github.com>
2018-04-03 21:33:28 +0300

    Add files via upload

--- js/tendons.js
+++ js/tendons.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	// fade-in for the logo and initial one for the link
 	$('#sub_slogan').animate({opacity:'1.0'},2000);
-	$('#emblem').animate({opacity:'1.0'},4000);
+	$('#main_explanation').animate({opacity:'1.0'},4000);
 	// ----------------------------------
 
 });
